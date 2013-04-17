d0b01ff2bd8b209c8b814559b186f3a420c6713f
Marco Rojo <mrmarcondes@gmail.com>
2013-04-17 16:04:01 -0300

    versao inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pusher::Application.config.secret_key_base = 'befb632327793f70006c13d87f3794a623642eef0aa71e44bd726c32fb78d81b4742b896769d9047c6a69d40d96710aaa9ad4f5f8bc865298e2d39662732e212'
